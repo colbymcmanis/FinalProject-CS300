3bcaabf5f1884c81a9d15a6974a2a875c8102784
Colby McManis <colbymcmanis01@gmail.com>
2023-12-10 17:12:24 -0600

    Added Login Page and Transfer Abillity

--- routes/inventory.js
+++ routes/inventory.js
@@ -28,29 +28,14 @@ try {
 }
 */
 
-var warehouseInventory = [
-{
-    warehousenumber: 1,
-    warehouselocation: "Kansas",
-    pepsi: 987,
-    mountaindew: 843,
-    starry: 999,
-    cocacola:556,
-    drpepper: 764,
-    doc360: 431,
-    fanta: 834,
-    sprite: 342,
-    water: 713,
-    gatorade: 253,
-    redbull: 398,
-},
-];
+
+
 
 
 
 var inventory = await inventoryschema.find();
 
-res.render('inventory',{ inventory: inventory, warehouseInventory: warehouseInventory});
+res.render('inventory',{ inventory: inventory});
 
 });
 
@@ -58,11 +43,76 @@ router.get('/add', function(req, res, next) {
     res.render('addstore');
 });
 
-router.get('/add', function(req, res, next) {
-    res.render('addstore');
+router.get('/transfer', function(req, res, next) {
+
+    let id = req.query._id;
+
+    let inventory = inventoryschema.findById(id);
+    let warehouseinventory = inventoryschema.findById('65763fdc7bf22573c1efb38b');
+
+
+    res.render('transfer', {inventory: inventory, id: id});
 });
 
-router.post('/add', async function(req, res, next) {
+router.post('/transfer', async function(req, res, next) {
+    console.log(req.body.pepsi);
+ 
+    const inventory = await inventoryschema.findById(req.body._id);
+    const warehouseinventory = await inventoryschema.findById('65763fdc7bf22573c1efb38b');
+
+   
+    console.log(inventory.pepsi);
+    console.log(req.body._id);
+
+    await inventoryschema.findByIdAndUpdate({_id: req.body._id}, {
+       pepsi: Number(inventory.pepsi) + Number(req.body.pepsi),
+        mountaindew: Number(inventory.mountaindew) + Number(req.body.mountaindew),
+        starry: Number(inventory.starry) + Number(req.body.starry),
+        cocacola: Number(inventory.cocacola) + Number(req.body.cocacola),
+        drpepper: Number(inventory.drpepper) + Number(req.body.drpepper),
+        doc360: Number(inventory.doc360) + Number(req.body.doc360),
+        fanta: Number(inventory.fanta) + Number(req.body.fanta),
+        sprite: Number(inventory.sprite) + Number(req.body.sprite),
+        water: Number(inventory.water) + Number(req.body.water),
+        gatorade: Number(inventory.gatorade) + Number(req.body.gatorade),
+        redbull: Number(inventory.redbull) + Number(req.body.redbull),
+
+    
+
+
+        
+    });
+
+    await inventoryschema.findByIdAndUpdate({_id: '65763fdc7bf22573c1efb38b'}, {
+        pepsi: Number(warehouseinventory.pepsi) - Number(req.body.pepsi),
+        mountaindew: Number(warehouseinventory.mountaindew) - Number(req.body.mountaindew),
+        starry: Number(warehouseinventory.starry) - Number(req.body.starry),
+        cocacola: Number(warehouseinventory.cocacola) - Number(req.body.cocacola),
+        drpepper: Number(warehouseinventory.drpepper) - Number(req.body.drpepper),
+        doc360: Number(warehouseinventory.doc360) - Number(req.body.doc360),
+        fanta: Number(warehouseinventory.fanta) - Number(req.body.fanta),
+        sprite: Number(warehouseinventory.sprite) - Number(req.body.sprite),
+        water: Number(warehouseinventory.water) - Number(req.body.water),
+        gatorade: Number(warehouseinventory.gatorade) - Number(req.body.gatorade),
+        redbull: Number(warehouseinventory.redbull) - Number(req.body.redbull),
+
+    });
+
+
+    res.redirect('/inventory');
+}, function(err, result) {
+    if (err) {
+        console.log(err);
+    } else {
+        res.redirect('/inventory');
+    }
+});
+
+
+
+
+router.post('/add', async function(req, res, next) { 
+    console.log(formdata)
     var inventory = new inventoryschema({
         _id: req.body.id,
         storenumber: req.body.storenumber,  
